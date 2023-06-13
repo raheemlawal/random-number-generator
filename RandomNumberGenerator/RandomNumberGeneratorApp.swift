15d8e4a8247dcef39db5e19bdbd3f67e38208b64
Raheem Lawal <lawalraheem@gmail.com>
2023-06-12 23:26:04 -0700

    Initial Commit

---
+++ RandomNumberGenerator/RandomNumberGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomNumberGeneratorApp.swift
+//  RandomNumberGenerator
+//
+//  Created by Raheem Lawal on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RandomNumberGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
